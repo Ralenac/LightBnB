ea35f24202a65c6a1948325991f7bfb611116a7d
Ralenac <t.ralenac@gmail.com>
2021-10-14 14:35:39 -0400

    Update the getAllReservations function to use the lightbnb database with SQL queries

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,6 +1,15 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
 /// Users
 
 /**
@@ -12,14 +21,13 @@ const getUserWithEmail = function(email) {
   return pool
   .query(`SELECT * FROM users WHERE users.email = $1`, [email])
   .then((result) => {
-    console.log(result.rows);
+    return result.rows[0];
   })
   .catch((err) => {
     console.log(err.message);
   });
 }
 exports.getUserWithEmail = getUserWithEmail;
-
 
 
 /**
@@ -31,7 +39,7 @@ const getUserWithId = function(id) {
   return pool
   .query(`SELECT * FROM users WHERE users.id = $1`, [id])
   .then((result) => {
-    console.log(result.rows);
+    return result.rows[0];
   })
   .catch((err) => {
     console.log(err.message);
@@ -49,13 +57,14 @@ const addUser =  function(user) {
   return pool
   .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`, [user])
   .then((result) => {
-    console.log(result.rows);
+    return result.rows[0];
   })
   .catch((err) => {
     console.log(err.message);
   });
 }
 exports.addUser = addUser;
+
 
 /// Reservations
 
@@ -65,9 +74,28 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`SELECT reservations.*, properties.*, AVG(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN reservations ON properties.id = reservations.property_id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY reservations.id, properties.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `, [guest_id, limit])
+  .then((result) => {
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log("get all reservations err");
+    console.log(err.message);
+  });
 }
 exports.getAllReservations = getAllReservations;
+
+
 
 /// Properties
 
@@ -75,7 +103,7 @@ const getAllProperties = (options, limit = 10) => {
   return pool
     .query(`SELECT * FROM properties LIMIT $1`, [limit])
     .then((result) => {
-      console.log(result.rows);
+      return result.rows;
     })
     .catch((err) => {
       console.log(err.message);
